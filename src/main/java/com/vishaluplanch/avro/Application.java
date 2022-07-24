fa32c8f7bf25cf86338133e0d8e967d5f95d06f1
nimitadesh <nimita.ahs@gmail.com>
2022-07-24 13:49:25 -0700

    moved Application.java to outside the kafka package

--- src/main/java/com/vishaluplanch/avro/Application.java
+++ src/main/java/com/vishaluplanch/avro/Application.java
@@ -1,4 +1,4 @@
-package com.vishaluplanch.avro.kafka;
+package com.vishaluplanch.avro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
